78ab888e2b45ddf2bc0c51789748cc0dea0e5bd4
Corey Schneider <cschne11@uncc.edu>
2020-05-01 03:58:41 +0000

    Downgraded jquery from 3.5.0 to 3.4.1 - Fix for nav bar dropdowns

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,11 @@ require("custom/openingTimes.jquery")
 require("custom/openingTimes.jquery.min")
 //require("custom/test")
 
+//new
+import 'jquery'
+import 'popper.js'
+require("jquery")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
