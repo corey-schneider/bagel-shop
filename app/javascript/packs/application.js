906f3fde528153967bd99986d4f5564e138135e0
Corey Schneider <cschne11@uncc.edu>
2020-05-01 03:59:48 +0000

    Cleaned up application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,11 +12,6 @@ require("custom/openingTimes.jquery")
 require("custom/openingTimes.jquery.min")
 //require("custom/test")
 
-//new
-import 'jquery'
-import 'popper.js'
-require("jquery")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
