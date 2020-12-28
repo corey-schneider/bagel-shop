27b96bdab30c8949ec1fa5da06e954cad6deb0a2
Corey Schneider <cschne11@uncc.edu>
2020-12-28 06:15:08 +0000

    testing

--- config/puma.rb
+++ config/puma.rb
@@ -42,7 +42,7 @@ plugin :tmp_restart
 # Change to match your CPU core count
 workers 1
 
-# Min and Max threads per worker
+# Min and Max threads per worker 
 threads 1, 6
 
 app_dir = File.expand_path("../..", __FILE__)
