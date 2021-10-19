6e6bf84b0ebc54723d83f6b61473d8530cbdee64
Corey Schneider <coreyschneider2@gmail.com>
2021-10-19 18:39:09 -0400

    added ahoy; fixed google ads

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	belongs_to :user
+	visitable :ahoy_visit
 	
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :text, presence: true, length: { minimum: 5 }
