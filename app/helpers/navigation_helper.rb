ead067ddba66f9391aae6dd024be7e44332e1e7c
Corey Schneider <coreyschneider2@gmail.com>
2022-01-25 23:07:39 -0500

    cleaned + DRYed up navbar + hopefully support mobile devices

---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,27 @@
+# DRY navigation links
+
+module NavigationHelper
+  def nav_link(text, path)
+    content_tag(:li, class: 'nav-item ' + ('active' if current_page?(path)).to_s) do
+      link_to text, path, class: 'nav-link'
+    end
+  end
+
+  # For regular nav links on desktop
+  def old_nav_link_to(text, path)
+    if current_page?(path)
+      link_to text, path, class: 'nav-link active'
+    else
+      link_to text, path, class: 'nav-link'
+    end
+  end
+
+  # For 'mini' nav links on mobile devices
+  def old_mini_nav_link_to(text, path)
+    if current_page?(path)
+      link_to text, path, class: 'block text-sm px-2 py-4 text-white bg-green-500 font-semibold'
+    else
+      link_to text, path, class: 'block text-sm px-2 py-4 hover:bg-green-500 transition duration-300'
+    end
+  end
+end
